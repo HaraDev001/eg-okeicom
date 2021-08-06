910db1d89694c83b5d3eb286a7ffed72e4988536
HaraDev001 <haradev001@gmail.com>
2021-08-07 05:30:08 +0900

    Upload project

---
+++ public/custom/datepicker.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#datepicker").datepicker({
+        dateFormat: "yy年mm月dd日",
+    });
+});
+$(function() {
+    $("#datepickers").datepicker({
+        dateFormat: "yy年mm月dd日",
+    });
+});
+
+$('input').attr('autocomplete','off');
